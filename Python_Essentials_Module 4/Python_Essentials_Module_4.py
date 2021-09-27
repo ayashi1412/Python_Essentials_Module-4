22c79ca55a95d4f3f9242f7a8e7a69b158ef8721
Binod <ilikeit_12@hotmail.com>
2021-09-27 17:34:13 +0100

    Learp Year

--- Python_Essentials_Module 4/Python_Essentials_Module_4.py
+++ Python_Essentials_Module 4/Python_Essentials_Module_4.py
@@ -1,1 +1,23 @@
+#Created by: Binod Gurung
+#Date Created: 27 Sep 2021
+#Date last modified: 27 Sep 2021
 
+#LAB 4.3.1.6: Utilising parameterized funcitons and return statement to test leap year.
+
+def is_year_leap(year):
+#   
+    if(year % 4 == 0):
+        return True
+      
+#
+
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+for i in range(len(test_data)):
+	yr = test_data[i]
+	print(yr,"->",end="")
+	result = is_year_leap(yr)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
